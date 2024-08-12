62b7ed39a89d1ea030481227baad85818786247e
Dan Nguyen <pedantic@gmail.com>
2024-08-11 23:00:53 -0500

    Refactor to remove some functions

--- app/Main.hs
+++ app/Main.hs
@@ -10,19 +10,15 @@ import qualified Text.Read as TR
 main :: IO ()
 main = do
     args <- SE.getArgs
-    let width = Maybe.fromMaybe 30 . parseArgs $ args
-    mappingLines <- readLines "mapping"
+    mappingLines <- fmap lines . SIO.readFile $ "mapping"
     inputLines <- fmap lines $ getContents
+    let width = Maybe.fromMaybe 30 . parseArgs $ args
     let asciiMap = buildMap mappingLines
     -- M.forM_ inputLines (\x -> printPair (pad asciiMap width (replaceString asciiMap x)))
     M.forM_ inputLines (printPair . pad asciiMap width . replaceString asciiMap)
 
-
 parseArgs :: [String] -> Maybe Int
-parseArgs = firstJust isWidth
-
-firstJust :: (a -> Maybe b) -> [a] -> Maybe b
-firstJust f = Maybe.listToMaybe . Maybe.mapMaybe f
+parseArgs = Maybe.listToMaybe . Maybe.mapMaybe isWidth
 
 isWidth :: String -> Maybe Int
 isWidth ('-':'-':'w':'i':'d':'t':'h':'=':w) = TR.readMaybe w
@@ -30,9 +26,6 @@ isWidth _ = Nothing
 
 printPair :: (String, String) -> IO ()
 printPair (a, b) = putStr a >> putStrLn b
-
-readLines :: String -> IO [String]
-readLines = fmap lines . SIO.readFile
 
 buildMap :: [String] -> Map.Map Char Char
 buildMap = Map.fromList . map getMapping
@@ -44,11 +37,7 @@ getMapping _ = error "invalid input"
 replaceString :: Map.Map Char Char -> String -> String
 replaceString m s = foldr (\c a -> (Map.findWithDefault 'x' c m):a) "" s
 
-makePadding :: Int -> String
-makePadding n = take n . repeat $ ' '
-
 pad :: Map.Map Char Char -> Int -> String -> (String, String)
 pad m n s = (s, replaceString m p)
     where
-        l = length s
-        p = makePadding (n - l)
+        p = take (n - length s) . repeat $ ' '
